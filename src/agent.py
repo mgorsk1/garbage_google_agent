9c627ca9cfd733ed5861aad800d9ba1e435d1ee2
mgorsk1 <gorskimariusz13@gmail.com>
2025-06-07 13:03:26 +0200

    fix typos

--- src/agent.py
+++ src/agent.py
@@ -1,4 +1,5 @@
 from google.adk.agents.llm_agent import LlmAgent
+
 from .prompt import ROOT_GARBAGE_AGENT_PROMPT
 from .constants import MODEL
 from .sub_agents.warsaw.agent import warsaw_garbage_agent
@@ -8,7 +9,7 @@ load_dotenv()
 root_agent = LlmAgent(
     model=MODEL,
     name="tony_soprano",
-    description="An agent that herlps users with garbage collection queries.",
+    description="An agent that helps users with garbage collection queries.",
     instruction=ROOT_GARBAGE_AGENT_PROMPT,
     sub_agents=[warsaw_garbage_agent],
 )
